bb0e59c23b9301be0760854216703b82a71ecbc0
AldairRev <aldairreva@gmail.com>
2020-08-01 10:51:03 -0500

    init react project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+
+
+    render() {
+        return (
+            <div className="page-layout">
+                <div className="test" contentEditable>
+                    Aldair
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
